82462b53fce0c961ee58a05a3444a7a53619df78
sobuj301 <sobuj42301@gmail.com>
2025-09-03 15:26:36 +0600

    firstday and second day problem fixed

--- day1.js
+++ day1.js
@@ -1,1 +1,47 @@
-console.log('connected')
+// Day 1:
+
+// Print “Hello, World!”
+console.log('hello world')
+
+
+// Add two numbers from user input.
+const num1 = 5 ;
+const num2 = 6 ;
+
+const sum = num1 + num2 ;
+console.log(sum);
+
+
+// Swap two numbers without a temporary variable.
+
+let a = 10 ;
+let b = 15 ;
+
+a = a + b;
+b = a - b;
+a = a - b;
+
+
+console.log("a=" + a , "b ="+ b)
+
+// Check if a number is even or odd.
+
+const number = 5;
+if(number % 2 !== 0){
+    console.log('Odd')
+}
+else{
+    console.log("Even")
+}
+
+
+
+// Find the remainder of division of two numbers.
+
+const amount = 101;
+const person =  5;
+
+const remainder = amount % person;
+
+console.log(remainder)
+
