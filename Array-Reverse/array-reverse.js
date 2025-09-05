4e5e8949a149900eb87b0821d8eba157814ee69a
Md Sobuj Rana <sobuj42301@gmail.com>
2025-09-05 11:42:22 +0600

    array reverse using loop

---
+++ Array-Reverse/array-reverse.js
@@ -0,0 +1,23 @@
+
+// Reverse an array (without using built-in methods)
+// Remove duplicates from an array
+// Count how many times a number appears in an array
+// Create a new array with all elements doubled
+
+
+// Reverse an array (using loop)
+
+const friends = ["Green", "Jonh", "Red", "Yellow"]
+const reverse = [];
+// option-1
+for(let i = 0; i < friends.length; i++){
+    reverse.unshift(friends[i])
+}
+console.log(reverse)
+
+
+// option-2
+for(const friend of friends){
+    reverse.unshift(friend)
+}
+console.log(reverse)
